b25ae58b28e51cd76d40e8d24122cf9a90ff3115
AhmadImam14 <ahmad0903998@gmail.com>
2026-08-07 10:11:33 +0100

    making changes to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="container">
-        <p>© 2026 My Name</p>
+        <p>© 2026 Ahmad Imam</p>
         <p>Built with React & Vite</p>
       </div>
     </footer>
